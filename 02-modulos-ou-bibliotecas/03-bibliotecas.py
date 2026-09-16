eaa051e8e3c650dadef55c5dac496db238257125
Eduarda Schiefelbein <dudaschiefelbein@gmail.com>
2026-09-16 10:41:13 -0300

    Add files via upload

---
+++ 02-modulos-ou-bibliotecas/03-bibliotecas.py
@@ -0,0 +1,8 @@
+import math
+
+nota=6.855
+
+print(round(nota, 2))
+print(math.floor(nota))
+print(math.ceil(nota))
+print(math.pi)
---
+++ 02-modulos-ou-bibliotecas/04-bibliotecas.py
@@ -0,0 +1,13 @@
+import math
+import random
+
+nota=6.855
+
+#print(round(nota, 2))
+#print(math.floor(nota))
+#print(math.ceil(nota))
+#print(math.pi)
+
+print(round(random.random()*100))
+print(random.randint(1,20))
+
